190bc2c3c474a5c5c6925e62bc4a1fcc05e8ce40
Tim Chamberlin <tchamber@bates.edu>
2015-05-28 09:01:04 -0400

    update 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-  $('#nav_toggle').click(function(){
-    $('#main_content').hasClass("menu_open")
+
+  $("#nav_toggle").click( function(event){
+      event.preventDefault();
+      if ( $('#main_content').hasClass("isDown") ) {
+          $("#nav").stop().animate({marginTop:"-100px"}, 200);
+      } else {
+          $("#nav").stop().animate({marginTop:"0px"}, 200);
+      }
+      $(this).toggleClass("isDown");
+      return false;
   });
 });
